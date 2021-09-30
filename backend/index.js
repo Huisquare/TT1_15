186ff0a426fe57dff15af90ade8fa9ec93b3109d
jan-99 <jthong008@e.ntu.edu.sg>
2021-09-30 15:41:02 +0800

    get data from local database

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
-require('dotenv').config();
-const passport = require('passport')
-const LocalStrategy = require('passport-local').Strategy;
+const cors = require("cors");
+require("dotenv").config();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 /*Saving Session*/
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(passport.initialize());
-app.use(session({
-  secret: 'dbs',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "dbs",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 /*Save Session User ID*/
 passport.serializeUser((user, done) => {
@@ -30,51 +33,67 @@ passport.deserializeUser((id, done) => {
 });
 
 /*Passport Authentication*/
-passport.use(new LocalStrategy(
-  function(username, password, done) {
+passport.use(
+  new LocalStrategy(function (username, password, done) {
     db.customers.findOne({ username: username }, (err, user) => {
-      if (err) { return done(err); }
+      if (err) {
+        return done(err);
+      }
       if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
+        return done(null, false, { message: "Incorrect username." });
       }
       if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
+        return done(null, false, { message: "Incorrect password." });
       }
       return done(null, user);
     });
-  }
-));
+  })
+);
 
 /* MongoDB connection */
-const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://username1:username1password@cluster0.5sq7h.mongodb.net/dbs?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const { MongoClient } = require("mongodb");
+const uri =
+  "mongodb+srv://username1:username1password@cluster0.5sq7h.mongodb.net/dbs?retryWrites=true&w=majority";
+/* const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const db = client.db("dbs");
   console.log(db)
   // perform actions on the collection object
   client.close();
-});
-
+}); */
+const PORT = 5000;
+mongoose
+  .connect(uri)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Connection is established and running on port: ${PORT}`)
+    )
+  )
+  .catch((err) => console.log(err.message));
 
 /* Login Function */
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/', failureRedirect: '/login', failureFlash: true })
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
 );
 
 /* Logout Function */
 app.get("/logout", (req, res) => {
-    req.logout();
-    res.redirect("/login");
-  });
+  req.logout();
+  res.redirect("/login");
+});
 
 /* Create User */
 app.post("create", (req, res) => {
-  var newuser = req.body.username
-  db.customers.findOne({username: newuser}, (err, userFound) => {
+  var newuser = req.body.username;
+  db.customers.findOne({ username: newuser }, (err, userFound) => {
     if (err) return console.log(err);
     if (userFound) {
-      res.send(`Username ${newuser} taken`)
+      res.send(`Username ${newuser} taken`);
     } else {
       const data = {
         username: req.body.username,
@@ -83,44 +102,43 @@ app.post("create", (req, res) => {
         last_name: req.body.last_name,
         postal_code: req.body.postal_code,
         gender: req.body.gender,
-        created_at: new Date().toISOString().split('T')[0]
-      }
+        created_at: new Date().toISOString().split("T")[0],
+      };
       db.customers.push((err, data) => {
         if (err) return console.log(err);
-        res.json({username})
-      })
+        res.json({ username });
+      });
     }
-  })
-})
+  });
+});
 
-const listener = app.listen(process.env.PORT || 4000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
-
+/* const listener = app.listen(process.env.PORT || 4000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+}); */
 
 /*
 let cart = sessionStorage /* Cart would be session storage*/
 
-/*View Shopping Cart*/ 
+/*View Shopping Cart*/
 /*
 app.get('cart/view', (req, res) => {
   res.send(sessionStorage)
 })
 
-/*Update Shopping Cart*/ 
+/*Update Shopping Cart*/
 /*
 app.get('cart/update', (req, res) => {
 
 })
 
-/*Add Shopping Cart*/ 
+/*Add Shopping Cart*/
 /*
 app.post('cart/add', (req, res) => {
   sessionStorage.push(req.body)
   res.send(sessionStorage)
 })
 
-/*Delete Shopping Cart*/ 
+/*Delete Shopping Cart*/
 /*
 app.get('cart/delete', (req, res) => {
   sessionStorage = sessionStorage.filter(item => {
@@ -129,7 +147,7 @@ app.get('cart/delete', (req, res) => {
   res.send(sessionStorage)
 })
 
-/* Updating Database upon Checkout */ 
+/* Updating Database upon Checkout */
 /*
 app.post('/checkout', (req, res) => {
   var prodID = req.body.id
@@ -140,32 +158,31 @@ app.post('/checkout', (req, res) => {
 
 /* View Products */
 app.get("/view/products", (req, res) => {
-  console.log('product')
-  db.products.find({}).then(prod => {
-  res.send(prod);
+  console.log("product");
+  db.products.find({}).then((prod) => {
+    res.send(prod);
   });
- })
+});
 
 /* View Categories */
 app.get("/view/categories", (req, res) => {
-  db.categories.find({}).then(cat => {
-  res.send(cat);
+  db.categories.find({}).then((cat) => {
+    res.send(cat);
   });
- })
+});
 
- 
- const data = require("./categories.json");
+const data = require("./categories.json");
 
- /*return categories*/
- app.get("/categories", (req, res) => {
-     res.header("Content-Type", "application/json");
-     res.send(JSON.stringify(data));
- });
+/*return categories*/
+app.get("/categories", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.send(JSON.stringify(data));
+});
 
- const productData = require("./products.json");
+const productData = require("./products.json");
 
- /*return products*/
- app.get("/products", (req, res) => {
-     res.header("Content-Type", "application/json");
-     res.send(JSON.stringify(productData));
- });
+/*return products*/
+app.get("/products", (req, res) => {
+  res.header("Content-Type", "application/json");
+  res.send(JSON.stringify(productData));
+});
