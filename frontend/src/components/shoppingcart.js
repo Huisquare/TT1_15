e11732a241b5af7000e97cfe017d74a4de62fe13
jietung <jietungchai@gmail.com>
2021-09-30 16:15:34 +0800

    add clear localstorage when checkout

--- frontend/src/components/shoppingcart.js
+++ frontend/src/components/shoppingcart.js
@@ -2,12 +2,35 @@ import React, { useState, useEffect } from "react";
 import CartItems from './cartItems';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Box from '@mui/material/Box';
+import LinearProgress from '@mui/material/LinearProgress';
 import data from "../products.json";
+import { Nav } from "react-bootstrap";
 
 export default function ShoppingCart() {
 
     const [exist, setExist] = useState(false);
     const [list, setList] = useState([]);
+    const [submitLoading, setSubmitLoading] = useState(false);
+    const [checkout, setCheckout] = useState(false);
+
+    const handleCheckout = () => {
+        localStorage.removeItem("products");
+        setCheckout(true);
+    }
+
+    const handleOpen = () => {
+        setCheckout(true);
+    }
+
+    const handleClose = () => {
+        setCheckout(false);
+    }
 
     useEffect(() => {
         let products = [];
@@ -37,19 +60,44 @@ export default function ShoppingCart() {
     return (
         <div>
             {!exist ? <Typography>Shopping cart is empty</Typography>
-            :<>
-            <Typography variant="h3" style={{ 'textAlign': 'left', 'paddingLeft': '50px' }}>ShoppingCart</Typography>
-            {list.map((dataItem, index) => (
-                <CartItems 
-                    key={index} 
-                    productId={dataItem.productId} 
-                    name={data[dataItem.productId].title}
-                    price={data[dataItem.productId].price}
-                    qty={dataItem.qty}
-                />    
-            ))}
-            <Button variant="contained">Checkout</Button>
-            </>}
+                : <>
+                    <Typography variant="h3" style={{ 'textAlign': 'left', 'paddingLeft': '50px' }}>ShoppingCart</Typography>
+                    {list.map((dataItem, index) => (
+                        <CartItems
+                            key={index}
+                            productId={dataItem.productId}
+                            name={data[dataItem.productId].title}
+                            price={data[dataItem.productId].price}
+                            qty={dataItem.qty}
+                        />
+                    ))}
+                    <Button onClick={() => handleOpen()} variant="contained">Checkout</Button>
+                    <Dialog open={checkout} onClose={() => handleClose()}
+                        BackdropProps={{ style: { backgroundColor: "rgba(0, 0, 0, 0.5)" } }}
+                        PaperProps={{ style: { boxShadow: "none", overflow: "hidden" } }}
+                    >
+                        <DialogTitle id="form-dialog-title">Checkout</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Confirm checkout?
+                            </DialogContentText>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                {submitLoading ?
+                                    <Box sx={{ width: '100%' }}>
+                                        <LinearProgress color="secondary" />
+                                    </Box> : <></>}
+                            </div>
+                        </DialogContent>
+                        <DialogActions>
+                            <Button onClick={handleClose} color="primary">
+                                Cancel
+                            </Button>
+                            <Button onClick={handleCheckout} color="primary">
+                                <Nav.Link href="/cart">Ok</Nav.Link>
+                            </Button>
+                        </DialogActions>
+                    </Dialog>
+                </>}
         </div>
     );
 }
