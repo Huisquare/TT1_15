349643a69d8b469c044ce9307f16bfe2da4206f3
jackphan96 <jack.phan.2018@sis.smu.edu.sg>
2021-09-30 12:52:17 +0800

    Navbar and Login set up

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import "../App.css";
+import axios from "axios";
+
+const Login = () => {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+  async function loginUser(credentials) {
+    axios
+      .post("http://localhost:8080/login", {
+        body: JSON.stringify(credentials)
+      })
+      .then((response) => {
+        console.log(response);
+      });
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username,
+      password,
+    });
+    // setToken(token);
+  };
+
+  return (
+    <div className="Login login-wrapper">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <p>Username</p>
+          <input type="text" onChange={(e) => setUserName(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
