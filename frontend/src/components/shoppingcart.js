85df474fe953b56092ce868630ef2e832bf496b8
jietung <jietungchai@gmail.com>
2021-09-30 11:50:15 +0800

    add packges

---
+++ frontend/src/components/shoppingcart.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Typography from '@mui/material/Typography';
+
+export default function ShoppingCart() {
+    return (
+        <div>
+            <Typography variant="h3">ShoppingCart</Typography>
+        </div>
+    );
+}
