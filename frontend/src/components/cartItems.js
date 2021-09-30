68fdd589a2ecb717818b5287c3b74d2fa047529e
jietung <jietungchai@gmail.com>
2021-09-30 13:46:48 +0800

    add get products from localstorage

--- frontend/src/components/cartItems.js
+++ frontend/src/components/cartItems.js
@@ -7,17 +7,18 @@ import Button from '@mui/material/Button';
 export default function CartItems(props) {
     // const title = props.title;
     // const price = props.price;
-    const title = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops";
+    // const title = "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops";
     const price = "109.95";
-    var quantity = 1;
+    const productId = props.productId;
+    const qty = props.qty;
     return (
         <Container style={{ 'display': 'flex', 'flexDirection': 'row', 'padding': '25px 0px 25px 0px' }}>
             <Container style={{ 'display': 'flex', 'flexDirection': 'column' }}>
-                <Typography variant="h6" style={{ 'textAlign': 'left' }}>{title}</Typography>
+                <Typography variant="h6" style={{ 'textAlign': 'left' }}>{productId}</Typography>
                 <Typography variant="h6" style={{ 'textAlign': 'left' }}>${price}</Typography>
             </Container>
             <Container style={{ 'display': 'flex', 'flexDirection': 'row', 'justifyContent': 'flex-end' }}>
-                <Typography variant="h6" style={{ 'textAlign': 'right', 'paddingRight': '10px' }}>{quantity}</Typography>
+                <Typography variant="h6" style={{ 'textAlign': 'right', 'paddingRight': '10px' }}>{qty}</Typography>
                 <Button variant="contained" style={{maxWidth: '60px', maxHeight: '35px', minWidth: '60px', minHeight: '35px'}}>Edit</Button>
             </Container>
         </Container>
