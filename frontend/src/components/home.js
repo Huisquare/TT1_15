331b4b703abc1d924e3d3fb91f570c591f60bf2c
jan-99 <jthong008@e.ntu.edu.sg>
2021-09-30 11:52:50 +0800

    add styling

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -1,5 +1,12 @@
 import React from "react";
 import products from "../products.json";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Typography from "@mui/material/Typography";
 
 const Home = () => {
   return (
@@ -7,14 +14,32 @@ const Home = () => {
       <h1>All Products</h1>
       {products.map((product) => {
         return (
-          <div key={product.id}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader title={product.title} />
+            <CardMedia
+              component="img"
+              alt={product.title}
+              height="140"
+              image={product.image}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {product.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Add To Cart</Button>
+            </CardActions>
+          </Card>
+          /*  <div key={product.id}>
             <h3>{product.title}</h3>
+            <img src={product.image} />
             <p>{product.description}</p>
             <p>${product.price}</p>
             <p>
               <button>Add to Cart</button>
             </p>
-          </div>
+          </div> */
         );
       })}
     </div>
