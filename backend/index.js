721fd0c4e49ba39d51124fa5abba5d48c17b12d6
alisudais <alisudais.suhaiemi@gmail.com>
2021-09-30 16:19:00 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+app.use(cors({
+  origin: '*'
+}));
 require("dotenv").config();
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
